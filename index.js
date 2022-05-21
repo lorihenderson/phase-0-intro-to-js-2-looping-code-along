11e61fdc6d2910204619765fbf782dc815537bf5
Lori Henderson <lorim95@gmail.com>
2022-05-20 22:06:16 -0400

    commit tree 6c8c15f0d8d8a278bb06f93ef0d27c893d8bb832

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    const msg = [];
+    for (let i = 0; i < names.length; i++) {
+        msg.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return msg;
+}
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        --num;
+    }
+    return  0;
+}
